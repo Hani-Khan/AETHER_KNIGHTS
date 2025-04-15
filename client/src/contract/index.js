5d4a779ef960fff65105debbbc1d39adf576cef8
Hani-Khan <hanikhan5352@gmail.com>
2025-04-15 08:52:43 +0500

    First commit

---
+++ client/src/contract/index.js
@@ -0,0 +1,11 @@
+import contract from './AVAXGods.json'
+
+export const contractAddress = "0xcf35F5749B7295Cb46177D68C0849bc7144cDFB3";
+export const { abi: contractABI } = contract;
+  
+
+// export const contractAddress = "0x4a4C73F7265b0C0b201f92D3d6c8ab58FfE4D3ae";
+//0x4a4C73F7265b0c0b201f92D3d6c8ab58FFE4D3ae ---> initial contract (nonchecksum)
+//0x4a4C73F7265b0C0b201f92D3d6c8ab58FfE4D3ae---> initial contract (checksum)
+
+//0x8dA21A380b05b0c37bD86a1c7eA45d13070bD55D ---> second deployed contract
